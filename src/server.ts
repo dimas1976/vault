7983d2aaf9160c95777067e97ada2d9407b6ade1
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-10 16:36:22 +0200

    ✨ create new feature

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,14 @@
 import { readCredentials } from './utils/credential';
+import express from 'express';
 
-readCredentials();
+const app = express();
+const port = 3000;
+
+app.get('/', (_req, res) => {
+  res.send(readCredentials());
+  //   res.send('HEllo');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
