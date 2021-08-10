1269f46fe4bc41533d30e7afc1de2052f048f377
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-10 14:07:09 +0200

    create basic db.json

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello Server!');
+import { readFile } from 'fs/promises';
+
+async function readPasswords() {
+  try {
+    console.log(await readFile('./src/db.json', 'utf-8'));
+  } catch (error) {
+    console.error('there was an error:', error.message);
+  }
+}
+
+readPasswords();
