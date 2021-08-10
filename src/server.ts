5809da54447b2af6e6288e6f8e648e34a98534ac
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-10 18:33:45 +0200

    ✨ Add new route

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
-import { readCredentials } from './utils/credential';
+import { getCredential, readCredentials } from './utils/credential';
 import express from 'express';
+import { Credential } from './types';
 
 const app = express();
 const port = 3000;
+
+app.get('/api/credentials', async (_req, res) => {
+  res.status(200).json(await readCredentials());
+});
+
+app.get('/api/credentials/credential', async (req, res) => {
+  console.log(req.params);
+  // const credential = await getCredential(req.params[0]);
+  // res.json(credential);
+});
 
 app.get('/', (_req, res) => {
   res.send(readCredentials());
