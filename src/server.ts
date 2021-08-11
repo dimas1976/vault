d3993f4a2caf9c53a58e0bbb48427415c9d3d006
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-11 18:05:56 +0200

    1)Add route to add a new credential 2) Add new route to add a credential

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,14 @@ import {
   getCredential,
   readCredentials,
   addCredential,
+  deleteCredential,
 } from './utils/credential';
+
 import express from 'express';
 
 const app = express();
 const port = 3000;
-
-addCredential({
-  service: 'Yahoo',
-  username: 'dimitri',
-  password: '1234',
-});
+app.use(express.json());
 
 app.get('/api/credentials', async (_req, res) => {
   try {
@@ -36,6 +33,26 @@ app.get('/api/credentials/:service', async (req, res) => {
 
 app.get('/', (_req, res) => {
   res.send(readCredentials());
+});
+
+app.post('/api/credentials', async (req, res) => {
+  try {
+    await addCredential(req.body);
+  } catch {
+    console.error(`Es wurde kein neues credential zugefügt`);
+    res.send('kein neues credential');
+  }
+});
+
+app.delete('/api/credentials/:service', async (req, res) => {
+  const { service } = req.params;
+  try {
+    await deleteCredential(service);
+    res.status(200).send();
+  } catch (error) {
+    console.error(error);
+    res.send(`Ups`);
+  }
 });
 
 app.listen(port, () => {
