07faa7f661d6800f8c2adf7e3aa8fa4c16cb0c32
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-11 12:03:59 +0200

    ✨ Create new route

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,31 @@
 import { getCredential, readCredentials } from './utils/credential';
 import express from 'express';
-import { Credential } from './types';
 
 const app = express();
 const port = 3000;
 
 app.get('/api/credentials', async (_req, res) => {
-  res.status(200).json(await readCredentials());
+  try {
+    res.status(200).json(await readCredentials());
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(`Internal ServerError`);
+  }
 });
 
-app.get('/api/credentials/credential', async (req, res) => {
-  console.log(req.params);
-  // const credential = await getCredential(req.params[0]);
-  // res.json(credential);
+app.get('/api/credentials/:service', async (req, res) => {
+  const { service } = req.params;
+  try {
+    const credential = await getCredential(service);
+    res.status(200).json(credential);
+  } catch (error) {
+    console.error(error);
+    res.status(404).send(`Could not find service: ${service}`);
+  }
 });
 
 app.get('/', (_req, res) => {
   res.send(readCredentials());
-  //   res.send('HEllo');
 });
 
 app.listen(port, () => {
