790fe41bd12e3d6af01c89501651732457c72d81
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-13 11:11:02 +0200

    Add keay as second parameter for decryptCredential method and for encryptCredential method

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,10 +1,13 @@
 import CryptoJS from 'crypto-js';
 import { Credential } from '../types';
 
-export function encryptCredential(credential: Credential): Credential {
+export function encryptCredential(
+  credential: Credential,
+  key: string
+): Credential {
   const encryptedCredentialPass = CryptoJS.TripleDES.encrypt(
     credential.password,
-    'neuefische'
+    key
   ).toString();
 
   const partiallyEncryptedCredential = {
@@ -15,10 +18,13 @@ export function encryptCredential(credential: Credential): Credential {
   return partiallyEncryptedCredential;
 }
 
-export function decryptCredential(credential: Credential): Credential {
+export function decryptCredential(
+  credential: Credential,
+  key: string
+): Credential {
   const decryptedPass = CryptoJS.TripleDES.decrypt(
     credential.password,
-    'neuefische'
+    key
   ).toString(CryptoJS.enc.Utf8);
 
   const partiallyDecryptedCredential = {
