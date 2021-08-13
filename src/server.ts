5732a5985688a2fe7c92d69e7458545d9a3e9eb5
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-13 11:07:08 +0200

    Add check, if  authorization is successfull or not

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import {
 } from './utils/credential';
 
 import express from 'express';
+import { validateMasterPasswort } from './utils/validation';
 
 const app = express();
 const port = 3000;
@@ -23,8 +24,17 @@ app.get('/api/credentials', async (_req, res) => {
 
 app.get('/api/credentials/:service', async (req, res) => {
   const { service } = req.params;
+  const masterPass = req.headers.authorization;
+  if (!masterPass) {
+    res.status(400).send('Authorization header missing');
+    return;
+  } else if ((await validateMasterPasswort(masterPass)) === false) {
+    res.status(401).send('Unathorized request');
+    return;
+  }
   try {
-    const credential = await getCredential(service);
+    const credential = await getCredential(service, masterPass);
+    console.log(credential);
     res.status(200).json(credential);
   } catch (error) {
     console.error(error);
@@ -33,11 +43,21 @@ app.get('/api/credentials/:service', async (req, res) => {
 });
 
 app.post('/api/credentials', async (req, res) => {
+  const masterPass = req.headers.authorization;
+  if (!masterPass) {
+    res.status(400).send('Authorization header missing');
+    return;
+  } else if ((await validateMasterPasswort(masterPass)) === false) {
+    res.status(401).send('Unathorized request');
+    return;
+  }
+
   try {
-    await addCredential(req.body);
+    console.log(req.body);
+    await addCredential(req.body, masterPass);
     res.json(req.body);
   } catch {
-    console.error(`It is no credential to delete`);
+    console.error(`There is no new credential`);
     res.send('There is no new credential');
   }
 });
