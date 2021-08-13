a0ec367e75d49441406c4cec005ae9b08dbe77f8
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-13 14:27:56 +0200

    🚚 Rename validateMasterPasswort

--- src/server.ts
+++ src/server.ts
@@ -7,18 +7,18 @@ import {
 } from './utils/credential';
 
 import express from 'express';
-import { validateMasterPasswort } from './utils/validation';
+import { validateMasterPassword } from './utils/validation';
 
 const app = express();
 const port = 3000;
 app.use(express.json());
 
 app.get('/api/credentials', async (req, res) => {
-  const masterPass = req.headers.authorization;
-  if (!masterPass) {
+  const masterPassword = req.headers.authorization;
+  if (!masterPassword) {
     res.status(400).send('Authorization header missing');
     return;
-  } else if (!(await validateMasterPasswort(masterPass))) {
+  } else if (!(await validateMasterPassword(masterPassword))) {
     res.status(401).send('Unathorized request');
     return;
   }
@@ -32,17 +32,16 @@ app.get('/api/credentials', async (req, res) => {
 
 app.get('/api/credentials/:service', async (req, res) => {
   const { service } = req.params;
-  const masterPass = req.headers.authorization;
-  if (!masterPass) {
+  const masterPassword = req.headers.authorization;
+  if (!masterPassword) {
     res.status(400).send('Authorization header missing');
     return;
-  } else if (!(await validateMasterPasswort(masterPass))) {
+  } else if (!(await validateMasterPassword(masterPassword))) {
     res.status(401).send('Unathorized request');
     return;
   }
   try {
-    const credential = await getCredential(service, masterPass);
-    console.log(credential);
+    const credential = await getCredential(service, masterPassword);
     res.status(200).json(credential);
   } catch (error) {
     console.error(error);
@@ -51,18 +50,17 @@ app.get('/api/credentials/:service', async (req, res) => {
 });
 
 app.post('/api/credentials', async (req, res) => {
-  const masterPass = req.headers.authorization;
-  if (!masterPass) {
+  const masterPassword = req.headers.authorization;
+  if (!masterPassword) {
     res.status(400).send('Authorization header missing');
     return;
-  } else if (!(await validateMasterPasswort(masterPass))) {
+  } else if (!(await validateMasterPassword(masterPassword))) {
     res.status(401).send('Unathorized request');
     return;
   }
 
   try {
-    console.log(req.body);
-    await addCredential(req.body, masterPass);
+    await addCredential(req.body, masterPassword);
     res.json(req.body);
   } catch {
     console.error(`There is no new credential`);
