cb44664e0c02999ee5547883b28d8769c4b419a0
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-15 18:27:43 +0200

    ✨ Add database function calls

--- src/utils/credential.ts
+++ src/utils/credential.ts
@@ -1,63 +1,53 @@
-import { readFile, writeFile } from 'fs/promises';
 import { DB, Credential } from '../types';
 import { decryptCredential, encryptCredential } from './crypto';
+import {
+  addCredentialToDB,
+  getCredentialFromDB,
+  getAllCredentialsFromDB,
+  deleteCredentialFromDB,
+  replaceCredentialDB,
+} from './database';
 
-export async function readCredentials(): Promise<Credential[]> {
-  const response = await readFile('./src/db.json', 'utf-8');
-  const db: DB = JSON.parse(response);
-  const credentials = db.credentials;
-  return credentials;
+export async function readCredentials(
+  masterPassword: string
+): Promise<Credential[]> {
+  const credentials: Credential[] = await getAllCredentialsFromDB();
+  return credentials.map((credential) =>
+    decryptCredential(credential, masterPassword)
+  );
 }
 
 export async function getCredential(
   service: string,
   masterPass: string
 ): Promise<Credential> {
-  const credentials = await readCredentials();
-  const filteredCredential = credentials.find((credential) => {
-    return credential.service === service;
-  });
-
-  if (!filteredCredential) {
-    throw new Error(`No credential: ${service}`);
-  }
-
+  const filteredCredential: Credential = await getCredentialFromDB(service);
   return decryptCredential(filteredCredential, masterPass);
 }
 
 export async function addCredential(
   credential: Credential,
   masterPass: string
-): Promise<void> {
-  const credentials = await readCredentials();
-  const updatedCredentials = [
-    ...credentials,
-    encryptCredential(credential, masterPass),
-  ];
-  await overwriteDB(updatedCredentials);
+): Promise<string> {
+  const encryptedCredential = encryptCredential(credential, masterPass);
+  return addCredentialToDB(encryptedCredential);
 }
 
-export async function deleteCredential(service: string): Promise<void> {
-  const credentials: Credential[] = await readCredentials();
-  const updatedCredentials = credentials.filter(
-    (credential) => credential.service !== service
-  );
-  if (credentials.length === updatedCredentials.length) {
-    throw new Error(`There is nothing to delete`);
+export async function deleteCredential(service: string): Promise<Credential> {
+  const deletedCredential = await deleteCredentialFromDB(service);
+  if (!deletedCredential) {
+    throw new Error(`Credential with name ${service} could not deleted`);
   }
-  await overwriteDB(updatedCredentials);
+  return deletedCredential;
 }
 
 export async function updateCredential(
   service: string,
-  credential: Credential
+  credential: Credential,
+  masterPassword: string
 ): Promise<void> {
-  const credentials = await readCredentials();
-  const filteredCredentials: Credential[] = credentials.filter(
-    (credential) => credential.service !== service
-  );
-  const updatedCredential: Credential[] = [...filteredCredentials, credential];
-  await overwriteDB(updatedCredential);
+  const encryptedCredential = encryptCredential(credential, masterPassword);
+  await replaceCredentialDB(service, encryptedCredential);
 }
 
 export async function overwriteDB(credential: Credential[]): Promise<void> {
