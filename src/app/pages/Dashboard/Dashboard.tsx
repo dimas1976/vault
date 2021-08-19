814a7465307c0217c78d70f61a960ecd750a5af5
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-19 12:08:47 +0200

    Create link

--- src/app/pages/Dashboard/Dashboard.tsx
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './Dashboard.module.css';
-
+import { Link } from 'react-router-dom';
 export default function Dashboard(): JSX.Element {
   return (
     <main className={styles.container}>
       <h1>Dashboard</h1>
       <p>Insert your passwort</p>
+      <Link to="/marwin">Zu Marwin</Link>
       <input type="text" />
       <div></div>
     </main>
