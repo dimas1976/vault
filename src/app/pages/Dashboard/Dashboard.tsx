5099f46db598aa44f18e06a85c0d4e99c5488ee5
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-19 09:59:18 +0200

    ♻️ Refactoring code

--- src/app/pages/Dashboard/Dashboard.tsx
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -3,13 +3,11 @@ import styles from './Dashboard.module.css';
 
 export default function Dashboard(): JSX.Element {
   return (
-    <>
-      <div className={styles.container}>
-        <h1>Dashboard</h1>
-        <p>Insert your passwort</p>
-        <input type="text" />
-        <div></div>
-      </div>
-    </>
+    <main className={styles.container}>
+      <h1>Dashboard</h1>
+      <p>Insert your passwort</p>
+      <input type="text" />
+      <div></div>
+    </main>
   );
 }
