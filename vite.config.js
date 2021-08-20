4fd0d623aa5c8ef494a236b182ae12175d65daff
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-20 11:56:36 +0200

    Configurate server port

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import dotenv from 'dotenv';
+dotenv.config();
+const PORT = process.env.PORT;
 
 export default defineConfig({
   plugins: [reactRefresh()],
+  server: {
+    proxy: {
+      '/api': {
+        target: `http://localhost:${PORT}`,
+        changeOrigin: true,
+      },
+    },
+  },
   build: {
     outDir: 'dist/app',
   },
