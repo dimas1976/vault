10fb4c03215d3e8fc1e7ad4f0d109dd3cdfa7009
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-21 12:22:46 +0200

    Add basic jsx structure for credential card

--- src/app/components/CredentialCard/CredentialCard.tsx
+++ src/app/components/CredentialCard/CredentialCard.tsx
@@ -1,5 +1,9 @@
+import { mdiPencil, mdiDelete } from '@mdi/js';
+import Icon from '@mdi/react';
 import React from 'react';
 import type { Credential } from '../../../types';
+import styles from './CredentialCard.module.css';
+import { Link } from 'react-router-dom';
 
 type CredentialCardProps = {
   credential: Credential;
@@ -8,10 +12,22 @@ export default function CredentialCard({
   credential,
 }: CredentialCardProps): JSX.Element {
   return (
-    <div>
-      <p>{credential.service}</p>
-      <p>{credential.username}</p>
-      <p>{credential.password}</p>
-    </div>
+    <article className={styles.credential}>
+      <h2>{credential.service}</h2>
+      <div className={styles.content}>
+        <div className={styles.content__data}>
+          <p>{credential.username}</p>
+          <p>{credential.password}</p>
+        </div>
+        <div className={styles.content__buttons}>
+          <Link to="">
+            <Icon className={styles.icon} path={mdiPencil} />
+          </Link>
+          <Link to="">
+            <Icon className={styles.icon} path={mdiDelete} />
+          </Link>
+        </div>
+      </div>
+    </article>
   );
 }
