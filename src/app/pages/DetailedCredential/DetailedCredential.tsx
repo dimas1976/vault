9603b40592b727e4df196a4b13b35906acd99f85
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-21 17:46:49 +0200

    Create detailed credential component

---
+++ src/app/pages/DetailedCredential/DetailedCredential.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './DetailedCredential';
+import CredentialCard from '../../components/CredentialCard/CredentialCard';
+
+export default function DetailedCredential(): JSX.Element {
+  return (
+    <main>{/* <CredentialCard credential /> */}I am detailed credential</main>
+  );
+}
