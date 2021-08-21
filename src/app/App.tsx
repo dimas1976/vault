1a64a63f0fdb872cb78542d421a35b03cad1dd63
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-21 17:42:44 +0200

    Create new following routes: Search credential page and detailed credential page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard/Dashboard';
 import NewCredentialPage from './pages/NewCredentialPage/NewCredentialPage';
+import SearchCredentialPage from './pages/SearchCredentialPage/SearchCredentialPage';
 import Password from './pages/Password/Password';
+import DetailedCredential from './pages/DetailedCredential/DetailedCredential';
 function App(): JSX.Element {
   return (
     <Router>
       <Switch>
         <Route path="/add">
           <NewCredentialPage />
+        </Route>
+        <Route path="/search">
+          <SearchCredentialPage />
+        </Route>
+        <Route path="/credential/:credentialId">
+          <DetailedCredential />
         </Route>
         <Route path="/service/:service">
           <Password />
