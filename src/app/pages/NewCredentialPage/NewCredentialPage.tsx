d6cb273b6df7600b563001806de8dd8552e31887
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-21 14:32:28 +0200

    Create functionality in frontend to save a new credential in DB

--- src/app/pages/NewCredentialPage/NewCredentialPage.tsx
+++ src/app/pages/NewCredentialPage/NewCredentialPage.tsx
@@ -1,15 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './NewCredentialPage.module.css';
 
 export default function NewCredentialPage(): JSX.Element {
+  const [service, setServiceName] = useState('');
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [masterPassword, setMasterPassword] = useState('');
+
+  async function sendNewCredentialToAPI() {
+    const newCredential = { service, username, password };
+    console.log(newCredential);
+    await fetch('/api/credentials', {
+      method: 'POST',
+      headers: {
+        Authorization: masterPassword,
+      },
+      body: JSON.stringify(newCredential),
+    });
+  }
   return (
     <main className={styles['new-credential']}>
       <h2>Add new Credential</h2>
-      <form className={styles['new-credential__form']}>
-        <input type="text" placeholder="service name" />
-        <input type="text" placeholder="user name" />
-        <input type="text" placeholder="password" />
-        <input type="text" placeholder="master password" />
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          sendNewCredentialToAPI();
+        }}
+        className={styles['new-credential__form']}
+      >
+        <input
+          type="text"
+          placeholder="service name"
+          value={service}
+          onChange={(event) => setServiceName(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="user name"
+          value={username}
+          onChange={(event) => setUserName(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="master password"
+          value={masterPassword}
+          onChange={(event) => setMasterPassword(event.target.value)}
+        />
         <button type="submit">Send</button>
       </form>
     </main>
