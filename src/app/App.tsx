b689e3ee2deb9f0843840104f5e90cdbd3cbd201
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-21 12:21:04 +0200

    Add neu  add route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard/Dashboard';
+import NewCredentialPage from './pages/NewCredentialPage/NewCredentialPage';
 import Password from './pages/Password/Password';
 function App(): JSX.Element {
   return (
     <Router>
       <Switch>
+        <Route path="/add">
+          <NewCredentialPage />
+        </Route>
         <Route path="/service/:service">
           <Password />
         </Route>
