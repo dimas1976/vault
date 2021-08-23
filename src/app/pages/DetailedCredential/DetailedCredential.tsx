41ce56c05d3d021381c3af56e3dcdd999301dbf3
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-23 16:42:26 +0200

    Remove detailed credential component

--- src/app/pages/DetailedCredential/DetailedCredential.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './DetailedCredential';
-import CredentialCard from '../../components/CredentialCard/CredentialCard';
-
-export default function DetailedCredential(): JSX.Element {
-  return (
-    <main>{/* <CredentialCard credential /> */}I am detailed credential</main>
-  );
-}
