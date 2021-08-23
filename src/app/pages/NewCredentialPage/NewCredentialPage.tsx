8093c898e7a8cf31a1aa85829722622b57b580c3
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-23 16:44:45 +0200

    Set types for statefull values

--- src/app/pages/NewCredentialPage/NewCredentialPage.tsx
+++ src/app/pages/NewCredentialPage/NewCredentialPage.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import styles from './NewCredentialPage.module.css';
 
 export default function NewCredentialPage(): JSX.Element {
-  const [service, setServiceName] = useState('');
-  const [username, setUserName] = useState('');
-  const [password, setPassword] = useState('');
-  const [masterPassword, setMasterPassword] = useState('');
+  const [service, setServiceName] = useState<string>('');
+  const [username, setUserName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [masterPassword, setMasterPassword] = useState<string>('');
+  const history = useHistory();
 
   async function sendNewCredentialToAPI() {
     const newCredential = { service, username, password };
-    console.log(newCredential);
     await fetch('/api/credentials', {
       method: 'POST',
       headers: {
@@ -18,7 +19,9 @@ export default function NewCredentialPage(): JSX.Element {
       },
       body: JSON.stringify(newCredential),
     });
+    history.push('/');
   }
+
   return (
     <main className={styles['new-credential']}>
       <h2>Add new Credential</h2>
@@ -34,24 +37,28 @@ export default function NewCredentialPage(): JSX.Element {
           placeholder="service name"
           value={service}
           onChange={(event) => setServiceName(event.target.value)}
+          required
         />
         <input
           type="text"
           placeholder="user name"
           value={username}
           onChange={(event) => setUserName(event.target.value)}
+          required
         />
         <input
           type="text"
           placeholder="password"
           value={password}
           onChange={(event) => setPassword(event.target.value)}
+          required
         />
         <input
           type="text"
           placeholder="master password"
           value={masterPassword}
           onChange={(event) => setMasterPassword(event.target.value)}
+          required
         />
         <button type="submit">Send</button>
       </form>
