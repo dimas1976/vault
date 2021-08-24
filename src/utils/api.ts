3d1e5ef173e4e7601d385b4f52ef6507dc26ceb9
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-24 09:21:45 +0200

    move updateCredentialInDB function

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -13,3 +13,18 @@ export async function sendNewCredentialToAPI(
     body: JSON.stringify(credential),
   });
 }
+
+export async function updateCredentialInDB(
+  serviceName: string,
+  masterPassword: string,
+  updatingCredential: Credential
+): Promise<void> {
+  await fetch(`/api/credentials/${serviceName}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: masterPassword,
+    },
+    body: JSON.stringify(updatingCredential),
+  });
+}
